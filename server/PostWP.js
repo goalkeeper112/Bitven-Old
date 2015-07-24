32786af7fd6b288dc52d06473cbfe3b63c62ad01
goalkeeper112 <goalkeeper112@(none)>
2015-07-24 18:00:37 +0000

    Nueva paleta de colores

--- server/PostWP.js
+++ server/PostWP.js
@@ -6,6 +6,22 @@ HTTP.get("https://public-api.wordpress.com/rest/v1.1/sites/bitven.wordpress.com/
 		}
 	});
 
+if(ServerSession.get("postsWP").found > Posts.find().count()){
+	ServerSession.get("postsWP").posts.forEach(function (post) {
+		if(Posts.findOne({post: post})){
+			return false;
+		}else{
+			return Posts.insert({post: post}, function(err, data){
+				if(err){
+					return err;
+				}else{
+					return true;
+				}
+			});
+		}
+	});
+}
+
 Meteor.startup(function(){
 	if(ServerSession.get("postsWP")){
 		if(Posts.find().count() === 0 && ServerSession.get("postsWP").found > 0){
